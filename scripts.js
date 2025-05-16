67baa28c9a2da708608f175d8baf32184c9d42d5
lennking <vikkerz456@gmail.com>
2025-05-15 17:30:06 -0700

    Implement bookshelf appending function

--- scripts.js
+++ scripts.js
@@ -8,8 +8,16 @@ function Book(title, author, pages, read) {
     this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary(someBook) {
-    myLibrary.push(someBook);
+function addBookToLibrary(thisBook) {
+    myLibrary.push(thisBook);
+    appendBookToShelf(thisBook);
+}
+
+const shelf = document.getElementById("shelf");
+function appendBookToShelf(book) {
+    const bookDiv = document.createElement('div');
+    bookDiv.textContent = `${book.title} by ${book.author}, ${book.pages} pages, Read: ${book.read}`;
+    shelf.appendChild(bookDiv);
 }
 
 let book1 = new Book("The Hobbit", "JRR Tolkien", 320, false);
