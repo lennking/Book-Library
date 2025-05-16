fb3e9b8de94664d330cfb62b810061922f3ffb32
lennking <vikkerz456@gmail.com>
2025-05-15 17:49:01 -0700

    Add form for new book

--- scripts.js
+++ scripts.js
@@ -8,17 +8,18 @@ function Book(title, author, pages, read) {
     this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary(thisBook) {
-    myLibrary.push(thisBook);
-    appendBookToShelf(thisBook);
-}
-
 const shelf = document.getElementById("shelf");
 function appendBookToShelf(book) {
     const bookDiv = document.createElement('div');
     bookDiv.textContent = `${book.title} by ${book.author}, ${book.pages} pages, Read: ${book.read}`;
     shelf.appendChild(bookDiv);
 }
+
+function addBookToLibrary(thisBook) {
+    myLibrary.push(thisBook);
+    appendBookToShelf(thisBook);
+}
+
 
 let book1 = new Book("The Hobbit", "JRR Tolkien", 320, false);
 addBookToLibrary(book1);
