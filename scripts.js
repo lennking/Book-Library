574eface2cd6bfd2d4c6c9d768ff6d25c1cfcf89
lennking <vikkerz456@gmail.com>
2025-05-17 00:00:57 -0700

    Implement button to add book

--- scripts.js
+++ scripts.js
@@ -9,6 +9,7 @@ function Book(title, author, pages, read) {
 }
 
 const shelf = document.getElementById("shelf");
+
 function appendBookToShelf(book) {
     const bookDiv = document.createElement('div');
     bookDiv.textContent = `${book.title} by ${book.author}, ${book.pages} pages, Read: ${book.read}`;
@@ -20,6 +21,19 @@ function addBookToLibrary(thisBook) {
     appendBookToShelf(thisBook);
 }
 
-
 let book1 = new Book("The Hobbit", "JRR Tolkien", 320, false);
 addBookToLibrary(book1);
+
+const form = document.getElementById('book-form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    //get book details
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.querySelector('input[name="read"]:checked').value==='true';
+    //create new book
+    const newBook = new Book(title, author, pages, read);
+    addBookToLibrary(newBook);
+
+});
